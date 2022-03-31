a33999cf150f8cf7e437ec7c4e9dff3edc646ae1
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-03-31 10:38:59 -0400

    Add files via upload

---
+++ Complete.cpp
@@ -0,0 +1,39 @@
+// Struct Notes
+
+
+#include <iostream>
+using namespace std;
+
+// A STRUCT is used to define an object
+// An OBJECT is a variable that holds multiple values
+
+struct Product {
+    string name;
+    double price;
+};
+
+// Product is the identifier for the struct
+// We can now make Product objects (variables)
+// Each Product will have a name and a price attribute
+
+
+int main()
+{
+    Product milk;
+    milk.name = "2% Milk";
+    milk.price = 2.99;
+
+    Product oreos;
+    oreos.name = "Double Stuff Oreos";
+    oreos.price = 3.50;
+
+    // You try: Make 3 more Products
+
+
+
+    // You try: Find the average price by adding the 5 prices together and dividing by 5
+
+
+    system("pause");
+}
+
